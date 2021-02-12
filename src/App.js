45f13a853949900ed47173d887bc1d22dcc1c270
Sameer Joshi <38069980+joshisameer343@users.noreply.github.com>
2021-02-12 15:46:50 +0530

    not correct city name error catch

--- src/App.js
+++ src/App.js
@@ -14,8 +14,19 @@ export default function App() {
       fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c496af33d732297c30b641b3b235e47`
       )
-        .then((response) => response.json())
-        .then((data) => setWeather(Math.floor(data.main.temp) - 273));
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            throw new Error("Enter correct city name");
+          }
+        })
+        .then((data) => setWeather(Math.floor(data.main.temp) - 273))
+
+        .catch((error) => {
+          console.log(error);
+          setError("Enter correct city name");
+        });
     }
   }
   return (
